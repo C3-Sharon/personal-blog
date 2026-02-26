b0e7b4bebf5e768d26b99b6b0cdd34c381dcd0ad
C3-Sharon <2985656204@qq.com>
2026-02-26 12:21:19 +0800

    refactor: 抽离跨域配置到 properties 文件，增强环境适配性

--- src/main/java/com/sharon/blog/config/CorsConfig.java
+++ src/main/java/com/sharon/blog/config/CorsConfig.java
@@ -1,16 +1,18 @@
 package com.sharon.blog.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-
+@Value("${app.cors.allowed-origin:http://localhost:5137}")
+    private String corsAllowedOrigin;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(corsAllowedOrigin)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
